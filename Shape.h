ee1d784de6557211bb6c134d1e9a66dfb3db2b58
Rick Umali <rickumali@gmail.com>
2010-04-03 14:08:56 +0000

    Revamp: Changed data from protected to private. Added more accessor methods to get at data.

--- Shape.h
+++ Shape.h
@@ -1,20 +1,28 @@
+#include <string>
+using std::string;
+
 #ifndef GUARD_shape_h
 #define GUARD_shape_h
 class Shape {
-    protected:
+    private:
         int shape_height[4];
         int shape_width[4];
-	    char *shape_data[4];
+        string shape_data[4];
     	int rotation; /* 0 to 3 */
-    	void rotate(int);
     public:
     	Shape();
     	virtual int get_piece_type() = 0;
-    	virtual int rotate_left() = 0;
-    	virtual int rotate_right() = 0;
+    	virtual void rotate_left() = 0;
+    	virtual void rotate_right() = 0;
     	virtual void draw() = 0;
-    	int get_height();
-    	int get_width();
-	char shapedata(int, int);
+    	virtual void init_shape(int, int, string []) = 0;
+    	int height() const;
+    	int width() const;
+    	int& height();
+    	int& width();
+        char shapedata(int, int) const;
+        char& shapedata(int, int);
+        string& shapedata();
+    	void rotate(int);
 };
 #endif
