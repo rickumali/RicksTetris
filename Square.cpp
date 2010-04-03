85da4d1e9bc1156fd8d61520a6c4cdac3b310921
Rick Umali <rickumali@gmail.com>
2010-04-03 14:17:51 +0000

    Revamped to use new initializer. Changed to use accessors throughout.

--- Square.cpp
+++ Square.cpp
@@ -6,40 +6,43 @@ using namespace std;
 
 /*
  * This piece is also known as "O", "package", "square", or "block".
+ *
+ * ##
+ * ##
+ *
+ * The simplest of them all, really.
  * 
  */
 Square::Square()
   : Shape ()
 {
-  shape_height[0] = 2;
-  shape_height[1] = 2;
-  shape_height[2] = 2;
-  shape_height[3] = 2;
-  shape_width[0] = 2;
-  shape_width[1] = 2;
-  shape_width[2] = 2;
-  shape_width[3] = 2;
-  shape_data[0] = "####";
-  shape_data[1] = "####";
-  shape_data[2] = "####";
-  shape_data[3] = "####";
+  string square[2] = 
+{ "##",
+  "##" };
+
+  init_shape(1, 2, square);
+  init_shape(1, 2, square);
+  init_shape(1, 2, square);
+  init_shape(1, 2, square);
 }
 
-int Square::rotate_left() 
+// TODO: Add an operator method to make "--" act like rotate_left()
+void Square::rotate_left() 
 {
   rotate(-1);
 }
 
-int Square::rotate_right() 
+// TODO: Add an operator method to make "++" act like rotate_right()
+void Square::rotate_right() 
 {
   rotate(1);
 }
 
 void Square::draw() 
 {
-  for (int row = 0; row < shape_height[rotation]; row++) {
-    for (int col = 0; col < shape_width[rotation]; col++) {
-      cout << shape_data[rotation][(row*shape_width[rotation])+col];
+  for (int row = 0; row < height(); row++) {
+    for (int col = 0; col < width(); col++) {
+      cout << shapedata(row, col);
     }
     cout << endl;
   }
@@ -48,4 +51,18 @@ void Square::draw()
 int Square::get_piece_type()
 {
   return(SQUARE);
+}
+
+void Square::init_shape(int rotation, int square_size, string in_shape_data[]) {
+	// get length of shape_data to get square_size
+	rotate(rotation);
+	height() = square_size;
+	width() = square_size;
+  
+	for (int i = 0; i < square_size; i++) {
+	    cout << "in_shape_data: " << in_shape_data[i] << endl;
+		shapedata() += in_shape_data[i];
+	}
+
+	cout << "shapedata(): " << shapedata() << endl;
 }
