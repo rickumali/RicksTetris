3fc25171f4f432421c6c0e6f5bac5e025a834f37
Rick Umali <rickumali@gmail.com>
2010-04-04 00:52:15 +0000

    Revamped! Made SDLGrid a standalone class that is "implemented-in-terms-of-a" Grid. Added appropriate getters/setters to access the new private data.

--- SDLGrid.h
+++ SDLGrid.h
@@ -1,10 +1,18 @@
 #include "SDL/SDL.h"
 #include "Grid.h"
+#include "SDLShape.h"
 
-class SDLGrid : public Grid {
+class SDLGrid {
   private:
     SDL_Surface *surface;
+	Grid *grid;
+	Uint32 shape_color;
   public:
     SDLGrid(SDL_Surface *);
-    void draw();
+    void place(int, int, SDLShape *);
+    bool out_of_bounds(int, int, SDLShape *);
+	void draw();
+	void set_grid(Grid *);
+	void set_surface(SDL_Surface *);
+	void set_shape_color(Uint32);
 };
