b1bb1f990203f91da7661e6564f4001227d723be
Rick Umali <rickumali@gmail.com>
2010-12-20 01:33:47 +0000

    Added a few more TODOs. Cleaned up code comments/formatting. Adding start.bmp to repo.

--- testgravity.cpp
+++ testgravity.cpp
@@ -61,8 +61,10 @@ int main( int argc, char* args[] )
 	return 1;
     }
 
+    // TODO: Generalize
     font=TTF_OpenFont("c:\\MinGW\\ttf-bitstream-vera-1.10\\VeraBd.ttf", 16);
     if (!font) {
+        // TODO: Test failure conditions	
         printf("TTF_OpenFont: %s\n", TTF_GetError());
     }
     
@@ -76,31 +78,33 @@ int main( int argc, char* args[] )
         return 1;    
     }
     
-    // XXX: Load the "background" bitmap
+    // Main "background" bitmap
     tetris_bmp = SDL_LoadBMP("tetris.bmp");
     if(tetris_bmp == NULL) {
        return(1);
     }
+    // Start "splash" "background" bitmap
     start_bmp = SDL_LoadBMP("start.bmp");
     if(start_bmp == NULL) {
        return(1);
     }
+    // Help "background" bitmap
     help_bmp = SDL_LoadBMP("help.bmp");
     if(help_bmp == NULL) {
        return(1);
     }
 
-    // BLIT the start "background" bitmap
+    // BLIT the start "splash" "background" bitmap
     SDL_BlitSurface(start_bmp , NULL, screen , NULL);
     SDL_UpdateRect(screen , 0 , 0 , 0 , 0 );
 
     //Set the window caption
     SDL_WM_SetCaption( "testgravity", NULL );
 
-	// Create the Grid
-	SDLGrid grid(screen);
+    // Create the Grid
+    SDLGrid grid(screen);
 
-    // Create a shape
+    // Create shapes
     SDLShape* square = new SDLSquare(screen);
     SDLShape* pyramid = new SDLPyramid(screen);
     SDLShape* leftslant = new SDLLeftSlant(screen);
@@ -119,51 +123,61 @@ int main( int argc, char* args[] )
     sb.addshape(leftell);
     sb.addshape(rightell);
 
-    SDLShape *selected_shape = sb.nextshape();
+    SDLShape *selected_shape;
 
-    // Starting location for ALL shapes
-    // int x = rand() % 11; // Return an x value between 0 and 10
-    int x = 5; // TODO: Modify this to account for shape!
-    int y = -1 * selected_shape->get_height();
-
-    int gravity = 1; // rows per second; can be fractional
+    int x, y; // Location of the shape on the grid
+    int gravity; // rows per second; TODO: Make it so it can be fractional
     int old_gravity = -1; // stores the gravity when the player wants to fast drop the shape
 
     // Scoring System
     ScoreSystem *scoring = new OriginalNintendoScoring();
-    int level = 0; // Need 10 rows cleared to increase level (and gravity)
-    int lines_cleared_in_level = 0;
+    int level; // Need 10 rows cleared to increase level (and gravity)
+    int lines_cleared_in_level;
 
     // Timer
     Timer fps;
     
-    bool user_quit = false; // User initiated the quit
-    bool game_quit = false; // Game initiated the quit (shapes all fall to the TOP)
-    bool pause = false;
-    bool start = false;
-    bool help = false;
-    int num_rows_to_clear = 0;
-    fps.start();
+    bool help; // User asked for 'help' Screen?
+    bool user_quit; // User initiated the quit
+    bool game_quit; // Game initiated the quit (shapes all fall to the TOP)
+    bool pause; // User asked to 'pause'
+    int num_rows_to_clear;
 
+    // The code block implements the start splash "screen"
+    bool start = false;
     while (start == false) {
-	 while (SDL_PollEvent(&event)) {
-  	   // This block of code implements "start game"
-  	   if (event.type != SDL_KEYDOWN) {
-  	   continue;
-  	   }
+      while (SDL_PollEvent(&event)) {
+  	if (event.type != SDL_KEYDOWN) {
+  	  continue;
+  	}
   
-  	   // This block of code implements "start game"
-  	   if (!start && event.key.keysym.sym != SDLK_s) {
-  	     continue;
-  	   } else {
-  	    start = true;
-  	   }
-	}
+  	if (!start && event.key.keysym.sym != SDLK_s) {
+  	  continue;
+  	} else {
+  	  start = true;
+  	}
+      }
     }
 
 start_game:
+    gravity = 1; old_gravity = -1;
+    game_quit = false; user_quit = false; start = true;
+    level = 0;
+    lines_cleared_in_level = 0;
+    pause = false; help = false;
+    num_rows_to_clear = 0;
+    fps.start();
+    grid.reset();
 
-    // BLIT the normal "background" bitmap
+    selected_shape = sb.nextshape(); // TODO: Reset the 7 shapes in the bag properly
+    // Starting location for ALL shapes
+    // int x = rand() % 11; // TODO: Return an x value between 0 and 10, that accounts for shape!
+    x = 5; // XXX: Hardcoded for now
+    y = -1 * selected_shape->get_height();
+
+    scoring->set_current_score(0);
+
+    // Now BLIT the normal "background" bitmap
     SDL_BlitSurface(tetris_bmp , NULL, screen , NULL);
 
     while (!user_quit && !game_quit) {
@@ -173,10 +187,10 @@ start_game:
 		    }
 
 		    if (event.type == SDL_KEYDOWN) {
-			        // This block of code implements "pause" (p unpauses)
+			        // This block of code implements "pause" (p turns pause on or off)
 				if (pause && event.key.keysym.sym != SDLK_p)
 				  continue;
-			        // This block of code implements "help" (h unlocks help)
+			        // This block of code implements "help" (h turns help on or off)
 				if (help && event.key.keysym.sym != SDLK_h)
 				  continue;
 
@@ -219,17 +233,17 @@ start_game:
 				    case SDLK_h:
 					    help = !help;
 					    if (help) {
-						    fps.pause();
+						fps.pause();
     						// BLIT the "help" bitmap (it's slightly 
 						// offset, to cover the lower-thirds game shapes)
 						SDL_Rect dest;
-  							dest.x = 250;
-  							dest.y = 250;
+  						dest.x = 250;
+  						dest.y = 250;
     						SDL_BlitSurface(help_bmp , NULL, screen , &dest);
 					    } else {
     						// BLIT the normal "background" bitmap
     						SDL_BlitSurface(tetris_bmp , NULL, screen , NULL);
-						    fps.unpause();
+						fps.unpause();
 					    }
 					    break;
 				    case SDLK_d:
@@ -254,8 +268,8 @@ start_game:
 		    if (level > 999999) {
                       level = 1000000; // Cap level at 1M
 		    }
-		    if (gravity > 20) {
-                      gravity = 20; // Cap gravity at 20
+		    if (gravity > 10) {
+                      gravity = 10; // Cap gravity at 10
 		    }
 		  }
 		}
@@ -311,38 +325,27 @@ start_game:
 	    }
     }
 
-    // Check if the game quit. If the game quit, then allow the user
-    // to start a new game. If the user quit, then just bail.
+    // Check if the game quit. If the game quit (because the pieces reach
+    // the top of the grid), then allow the user to start a new game. If the
+    // user quit, then just bail.
     if (game_quit) {
       user_quit = false; 
       write_gameover(screen);
       SDL_UpdateRect(screen , 0 , 0 , 0 , 0 );
       while (user_quit == false) {
-	   while (SDL_PollEvent(&event)) {
-  	     if (event.type != SDL_KEYDOWN) {
-  	       continue;
-  	     }
+	 while (SDL_PollEvent(&event)) {
+  	   if (event.type != SDL_KEYDOWN) {
+  	     continue;
+  	   }
 
-	     if (event.key.keysym.sym == SDLK_q) {
-	       user_quit = true;
-	     }
+	   if (event.key.keysym.sym == SDLK_q) {
+	     user_quit = true;
+	   }
   
-  	     if (event.key.keysym.sym == SDLK_n) {
-	       // Reset the game state
-               game_quit = false; user_quit = false; start = true;
-               level = 0;
-               lines_cleared_in_level = 0;
-               pause = false; help = false;
-               num_rows_to_clear = 0;
-               fps.start();
-	       grid.reset();
-               selected_shape = sb.nextshape(); // TODO: Reset the 7 shapes in the bag properly
-               x = 5; // TODO: Modify this to account for shape!
-	       y = -1 * selected_shape->get_height();
-               scoring->set_current_score(0);
-	       goto start_game; // XXX: This works, but REFACTOR!
-  	     }
-	  }
+  	   if (event.key.keysym.sym == SDLK_n) {
+	     goto start_game; // XXX: This works, but REFACTOR!
+  	   }
+	}
       }
     }
 
