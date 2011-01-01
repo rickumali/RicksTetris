6d81c2be0d0bce7318de6343109ed70ec62b7de2
Rick Umali <rickumali@gmail.com>
2011-01-01 00:45:05 +0000

    Removed the goto in the code.

--- tetris.cpp
+++ tetris.cpp
@@ -139,8 +139,8 @@ int main( int argc, char* args[] )
     
     // StatKeeper
     SDLStatKeeper statkeeper(screen, font);
-    statkeeper.clear_counters();
 
+    bool game_loop = true; // overall game loop
     bool help; // User asked for 'help' Screen?
     bool user_quit; // User initiated the quit
     bool game_quit; // Game initiated the quit (shapes all fall to the TOP)
@@ -165,209 +165,212 @@ int main( int argc, char* args[] )
       SDL_Delay(1); 
     }
 
-start_game:
-    gravity = 1; old_gravity = -1;
-    game_quit = false; user_quit = false; start = true;
-    level = 0;
-    lines_cleared_in_level = 0;
-    pause = false; help = false;
-    num_rows_to_clear = 0;
-    fps.start();
-    grid.reset();
-
-    selected_shape = sb.nextshape(); // TODO: Reset the 7 shapes in the bag properly
-    // Starting location for ALL shapes
-    // int x = rand() % 11; // TODO: Return an x value between 0 and 10, that accounts for shape!
-    x = 5; // XXX: Hardcoded for now
-    y = -1 * selected_shape->get_height();
-
-    // Track Shapes with StatKeeper
-    statkeeper.increment_shape_count(selected_shape->get_piece_type());
-
-    scoring->set_current_score(0);
-
-    // Now BLIT the normal "background" bitmap
-    SDL_BlitSurface(tetris_bmp , NULL, screen , NULL);
-
-    while (!user_quit && !game_quit) {
-	    while (SDL_PollEvent(&event)) {
-		    if (event.type == SDL_QUIT) {
-			    user_quit = true;
-		    }
-
-		    if (event.type == SDL_KEYDOWN) {
-			        // This block of code implements "pause" (p turns pause on or off)
-				if (pause && event.key.keysym.sym != SDLK_p)
-				  continue;
-			        // This block of code implements "help" (h turns help on or off)
-				if (help && event.key.keysym.sym != SDLK_h)
-				  continue;
-
-			    switch(event.key.keysym.sym) {
-				    case SDLK_UP:
-					    selected_shape->rotate_right();
-				        if (!grid.out_of_bounds(x,y,selected_shape)) {
-						} else {
-							selected_shape->rotate_left();
-						}
-					    break;
-				    case SDLK_DOWN:
-					    selected_shape->rotate_left();
-				        if (!grid.out_of_bounds(x,y,selected_shape)) {
-						} else {
-							selected_shape->rotate_right();
-						}
-					    break;
-				    case SDLK_LEFT:
-				        if (!grid.off_the_side(x - 1,y,selected_shape)) {
-				        	x--;
-						}
-					    break;
-				    case SDLK_RIGHT:
-				        if (!grid.off_the_side(x + 1,y,selected_shape)) {
-				        	x++;
-						}
-					    break;
-				    case SDLK_SPACE:
-					old_gravity = gravity;
-					gravity = 60;
-					break;
-				    case SDLK_p:
-					    pause = !pause;
-					    if (pause)
-						    fps.pause();
-					    else
-						    fps.unpause();
-					    break;
-				    case SDLK_h:
-					    help = !help;
-					    if (help) {
-						fps.pause();
-    						// BLIT the "help" bitmap (it's slightly 
-						// offset, to cover the lower-thirds game shapes)
-						SDL_Rect dest;
-  						dest.x = 250;
-  						dest.y = 223;
-    						SDL_BlitSurface(help_bmp , NULL, screen , &dest);
-					    } else {
-    						// BLIT the normal "background" bitmap
-    						SDL_BlitSurface(tetris_bmp , NULL, screen , NULL);
-						fps.unpause();
-					    }
-					    break;
-				    case SDLK_d:
-					    grid.debug_draw();
-					    break;
-				    case SDLK_q:
-					    user_quit = true;
-					    break;
+    while (game_loop) {
+	    gravity = 1; old_gravity = -1;
+	    game_quit = false; user_quit = false; start = true;
+	    level = 0;
+	    lines_cleared_in_level = 0;
+	    pause = false; help = false;
+	    num_rows_to_clear = 0;
+	    fps.start();
+	    grid.reset();
+	    statkeeper.clear_counters();
+	
+	    selected_shape = sb.nextshape(); // TODO: Reset the 7 shapes in the bag properly
+	    // Starting location for ALL shapes
+	    // int x = rand() % 11; // TODO: Return an x value between 0 and 10, that accounts for shape!
+	    x = 5; // XXX: Hardcoded for now
+	    y = -1 * selected_shape->get_height();
+	
+	    // Track Shapes with StatKeeper
+	    statkeeper.increment_shape_count(selected_shape->get_piece_type());
+	
+	    scoring->set_current_score(0);
+	
+	    // Now BLIT the normal "background" bitmap
+	    SDL_BlitSurface(tetris_bmp , NULL, screen , NULL);
+	
+	    while (!user_quit && !game_quit) {
+		    while (SDL_PollEvent(&event)) {
+			    if (event.type == SDL_QUIT) {
+				    user_quit = true;
+			    }
+	
+			    if (event.type == SDL_KEYDOWN) {
+				        // This block of code implements "pause" (p turns pause on or off)
+					if (pause && event.key.keysym.sym != SDLK_p)
+					  continue;
+				        // This block of code implements "help" (h turns help on or off)
+					if (help && event.key.keysym.sym != SDLK_h)
+					  continue;
+	
+				    switch(event.key.keysym.sym) {
+					    case SDLK_UP:
+						    selected_shape->rotate_right();
+					        if (!grid.out_of_bounds(x,y,selected_shape)) {
+							} else {
+								selected_shape->rotate_left();
+							}
+						    break;
+					    case SDLK_DOWN:
+						    selected_shape->rotate_left();
+					        if (!grid.out_of_bounds(x,y,selected_shape)) {
+							} else {
+								selected_shape->rotate_right();
+							}
+						    break;
+					    case SDLK_LEFT:
+					        if (!grid.off_the_side(x - 1,y,selected_shape)) {
+					        	x--;
+							}
+						    break;
+					    case SDLK_RIGHT:
+					        if (!grid.off_the_side(x + 1,y,selected_shape)) {
+					        	x++;
+							}
+						    break;
+					    case SDLK_SPACE:
+						old_gravity = gravity;
+						gravity = 60;
+						break;
+					    case SDLK_p:
+						    pause = !pause;
+						    if (pause)
+							    fps.pause();
+						    else
+							    fps.unpause();
+						    break;
+					    case SDLK_h:
+						    help = !help;
+						    if (help) {
+							fps.pause();
+	    						// BLIT the "help" bitmap (it's slightly 
+							// offset, to cover the lower-thirds game shapes)
+							SDL_Rect dest;
+	  						dest.x = 250;
+	  						dest.y = 223;
+	    						SDL_BlitSurface(help_bmp , NULL, screen , &dest);
+						    } else {
+	    						// BLIT the normal "background" bitmap
+	    						SDL_BlitSurface(tetris_bmp , NULL, screen , NULL);
+							fps.unpause();
+						    }
+						    break;
+					    case SDLK_d:
+						    grid.debug_draw();
+						    break;
+					    case SDLK_q:
+						    game_loop = false;
+						    user_quit = true;
+						    break;
+				    }
 			    }
 		    }
-	    }
-
-            if( fps.get_ticks() > (1000/gravity) )
-            {
-		if (num_rows_to_clear > 0) {
-                  (void) scoring->add_lines_to_score(level, num_rows_to_clear); 
-		  lines_cleared_in_level += num_rows_to_clear;
-		  num_rows_to_clear = 0;
-		  if (lines_cleared_in_level > 10) {
-		    level++; gravity++;
-		    lines_cleared_in_level = 0;
-		    if (level > 999999) {
-                      level = 1000000; // Cap level at 1M
-		    }
-		    if (gravity > 10) {
-                      gravity = 10; // Cap gravity at 10
-		    }
-		  }
-		}
-
-		if (!grid.at_bottom_or_on_mound(x,y+1,selected_shape)) {
-			y++;
-		} else {
-			if (old_gravity != -1) {
-				// This code resets the gravity after the user has done a fast drop.
-				gravity = old_gravity;
-				old_gravity = -1;
+	
+	            if( fps.get_ticks() > (1000/gravity) )
+	            {
+			if (num_rows_to_clear > 0) {
+	                  (void) scoring->add_lines_to_score(level, num_rows_to_clear); 
+			  lines_cleared_in_level += num_rows_to_clear;
+			  num_rows_to_clear = 0;
+			  if (lines_cleared_in_level > 10) {
+			    level++; gravity++;
+			    lines_cleared_in_level = 0;
+			    if (level > 999999) {
+	                      level = 1000000; // Cap level at 1M
+			    }
+			    if (gravity > 10) {
+	                      gravity = 10; // Cap gravity at 10
+			    }
+			  }
 			}
-			grid.add_to_mound(x,y,selected_shape);
-			if (y <= 0) {
-			  game_quit = true;
+	
+			if (!grid.at_bottom_or_on_mound(x,y+1,selected_shape)) {
+				y++;
 			} else {
-                          selected_shape = sb.nextshape();
-                          x = 5; // TODO: Modify this to account for shape!
-		          y = -1 * selected_shape->get_height();
-			  num_rows_to_clear = grid.get_num_rows_to_clear();
-			  statkeeper.increment_shape_count(selected_shape->get_piece_type());
-			}
-	        }
-		fps.start(); // Restart the clock
+				if (old_gravity != -1) {
+					// This code resets the gravity after the user has done a fast drop.
+					gravity = old_gravity;
+					old_gravity = -1;
+				}
+				grid.add_to_mound(x,y,selected_shape);
+				if (y <= 0) {
+				  game_quit = true;
+				} else {
+	                          selected_shape = sb.nextshape();
+	                          x = 5; // TODO: Modify this to account for shape!
+			          y = -1 * selected_shape->get_height();
+				  num_rows_to_clear = grid.get_num_rows_to_clear();
+				  statkeeper.increment_shape_count(selected_shape->get_piece_type());
+				}
+		        }
+			fps.start(); // Restart the clock
+		    }
+	
+		    scoring->write_score();
+		    if (!help) {
+		      statkeeper.write_stats();
+		    }
+		    write_level(screen, level+1);
+	
+		    /* Redraw "everything" */
+		    if ( SDL_MUSTLOCK(screen) ) {
+	    		if ( SDL_LockSurface(screen) < 0 ) {
+	    		    cout << "Can't lock screen:" << SDL_GetError() << std::endl;
+	    		    return 1;
+	    		}
+		    }
+	
+		    if (num_rows_to_clear > 0) {
+		      grid.draw(pause||help);
+		      grid.animate_rows_to_clear(fps.get_ticks());
+		    } else {
+		      grid.clear_rows();
+		      grid.place(x, y, selected_shape);
+		      grid.draw(pause||help);
+		    }
+	
+		    if (SDL_MUSTLOCK(screen)) {
+			SDL_UnlockSurface(screen);
+		    }
+	
+		    if (SDL_Flip(screen) == -1) {
+			    cout << "Problem with the SDL_Flip()" << std::endl;
+			    return 1;
+		    }
+	
+		    // See http://www.gamedev.net/community/forums/topic.asp?topic_id=360184
+	            SDL_Delay(1); 
 	    }
-
-	    scoring->write_score();
-	    if (!help) {
-	      statkeeper.write_stats();
+	
+	    scoring->write_score_to_file();
+	    // Check if the game quit. If the game quit (because the pieces reach
+	    // the top of the grid), then allow the user to start a new game. If the
+	    // user quit, then just bail.
+	    if (game_quit) {
+	      user_quit = false; 
+	      write_gameover(screen);
+	      SDL_UpdateRect(screen , 0 , 0 , 0 , 0 );
+	      while (user_quit == false) {
+		 while (SDL_PollEvent(&event)) {
+	  	   if (event.type != SDL_KEYDOWN) {
+	  	     continue;
+	  	   }
+	
+		   if (event.key.keysym.sym == SDLK_q) {
+		     user_quit = true;
+		     game_loop = false;
+		   }
+	  
+	  	   if (event.key.keysym.sym == SDLK_n) {
+		     user_quit = true; // XXX: This works, but REFACTOR!
+	  	   }
+		}
+		// See http://www.gamedev.net/community/forums/topic.asp?topic_id=360184
+	        SDL_Delay(1); 
+	      }
 	    }
-	    write_level(screen, level+1);
-
-	    /* Redraw "everything" */
-	    if ( SDL_MUSTLOCK(screen) ) {
-    		if ( SDL_LockSurface(screen) < 0 ) {
-    		    cout << "Can't lock screen:" << SDL_GetError() << std::endl;
-    		    return 1;
-    		}
-	    }
-
-	    if (num_rows_to_clear > 0) {
-	      grid.draw(pause||help);
-	      grid.animate_rows_to_clear(fps.get_ticks());
-	    } else {
-	      grid.clear_rows();
-	      grid.place(x, y, selected_shape);
-	      grid.draw(pause||help);
-	    }
-
-	    if (SDL_MUSTLOCK(screen)) {
-		SDL_UnlockSurface(screen);
-	    }
-
-	    if (SDL_Flip(screen) == -1) {
-		    cout << "Problem with the SDL_Flip()" << std::endl;
-		    return 1;
-	    }
-
-	    // See http://www.gamedev.net/community/forums/topic.asp?topic_id=360184
-            SDL_Delay(1); 
     }
-
-    scoring->write_score_to_file();
-    // Check if the game quit. If the game quit (because the pieces reach
-    // the top of the grid), then allow the user to start a new game. If the
-    // user quit, then just bail.
-    if (game_quit) {
-      user_quit = false; 
-      write_gameover(screen);
-      SDL_UpdateRect(screen , 0 , 0 , 0 , 0 );
-      while (user_quit == false) {
-	 while (SDL_PollEvent(&event)) {
-  	   if (event.type != SDL_KEYDOWN) {
-  	     continue;
-  	   }
-
-	   if (event.key.keysym.sym == SDLK_q) {
-	     user_quit = true;
-	   }
-  
-  	   if (event.key.keysym.sym == SDLK_n) {
-	     goto start_game; // XXX: This works, but REFACTOR!
-  	   }
-	}
-	// See http://www.gamedev.net/community/forums/topic.asp?topic_id=360184
-        SDL_Delay(1); 
-      }
-    }
-
     TTF_CloseFont(font);
     TTF_Quit();
 
