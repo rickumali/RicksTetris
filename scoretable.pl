c3051057b823a77af7dde6f9e951d1772027e4d8
Rick Umali <rickumali@gmail.com>
2011-01-16 01:50:37 +0000

    Wrote a small Perl script to print the scores.txt file with human-readable dates.

---
+++ scoretable.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+# scoretable.pl
+#
+# Takes the scores.txt from Rick Umali's Tetris, and dumps it with 
+# a human-readable time.
+
+# USAGE: ./scoretable.pl < scores.txt
+
+# The scores.txt file contains:
+# 1293244545 0
+# 1293245269 14260
+# 1293418884 2280
+# 1293419560 13500
+
+# The output of this program looks like:
+# 1: Fri Dec 24 21:35:45 2010     0
+# 2: Fri Dec 24 21:47:49 2010     14260
+# 3: Sun Dec 26 22:01:24 2010     2280
+# 4: Sun Dec 26 22:12:40 2010     13500
+$count = 0;
+while (<>) {
+	$count++;
+	@F = split(' ');
+	print "$count: " . localtime($F[0]) . "\t$F[1]\n";
+}
